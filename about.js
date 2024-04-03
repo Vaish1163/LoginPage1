5a2d23280fa05642c1cd39991d63390d80dc40e4
Vaish1163 <144944378+Vaish1163@users.noreply.github.com>
2024-04-03 18:48:06 +0530

    Create about.js

---
+++ about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+      <div>
+        <h1>About Page</h1>
+        <p>This is the About page content.</p>
+      </div>
+    );
+  };
+  
+  export default About;
+  
